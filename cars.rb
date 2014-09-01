838d5a04ef65e033ba447ea03d8cb297064bb2fc
Zhao Li <zhaol@hawaii.edu>
2014-09-01 10:19:49 +0000

    add array and hash

--- cars.rb
+++ cars.rb
@@ -10,11 +10,11 @@ class Cars
     @miles = miles # different with each new instance
   end
 
-  def color?
+  def color
     @color
   end
   
-  def miles?
+  def miles
     @miles
   end
   
--- main.rb
+++ main.rb
@@ -33,10 +33,26 @@ puts "Cars generally has #{Cars.wheels?} wheels"
 your_car = Cars.new('shiny blue', 0) # instantiate a new car
 my_car   = Cars.new('rusty brown', 100000) # instantiate an old car
 
-puts "your car is #{your_car.color?} and has #{your_car.miles?} miles"
-puts "my car is #{my_car.color?} and has #{my_car.miles?} miles"
+puts "your car is #{your_car.color} and has #{your_car.miles} miles"
+puts "my car is #{my_car.color} and has #{my_car.miles} miles"
 
 your_car.drive_for(1, 10000)
 my_car.drive_for(10, 50000)
 
-puts "your car now has #{your_car.miles?} and my car now has #{my_car.miles?}"
+puts "your car now has #{your_car.miles} and my car now has #{my_car.miles}"
+
+# array
+cars = [my_car, your_car]
+
+cars.each do |car|
+  puts "A #{car.color} car with #{car.miles} miles"
+end
+
+# hash
+accident = {
+  party_at_fault: your_car,
+  victim: my_car,
+  witnesses: [Cars.new('red', 100), Cars.new('green', 300), Cars.new('black', 500)]
+}
+
+puts "A #{accident[:party_at_fault].color} car has just hit a #{accident[:victim].color} car and there are #{accident[:witnesses].size} witnesses."
